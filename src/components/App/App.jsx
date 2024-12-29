63b7904466cbf5fd082607a3d8a1c37dfcb1cb19
BigGarrison <paxhuckstep@gmail.com>
2024-12-29 15:41:53 -0700

    part 5 done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from '../Header/Header'
+import Main from '../Main/Main'
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
      <div className='page'>
       <div className='page__content'>
         <Header />
+        <Main />
         </div>
       </div>
    
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,16 @@
 import "./Header.css"
-//import logo from "../../assets/logo.svg"
-//import avatar from "../../assets/avatar.png"
+import logo from "../../assets/logo.png"
+import avatar from "../../assets/avatar.png"
 function Header () {
     return (
          
         <header className="header">
-        {/* <img className="header__logo" src={logo} /> */}
+        <img className="header__logo" src={logo} />
         <p className="header__date-and-location">DATE, LOCATION</p>
         <button className="header__add-clothes-button">+ Add Clothes</button>
         <div className="header__user-container">
                 <p className="header__username">Terrence Tegegne</p>
-                {/* <img src={avatar} alt="Terrence Tegegne" className="header__avatar" /> */}
+                <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
             </div>
 
         </header>
