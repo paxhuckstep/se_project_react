6eddfa56ddccb7f4c7c576e666ddada4e77f6d8a
BigGarrison <paxhuckstep@gmail.com>
2024-12-29 17:38:20 -0700

    finished video 8

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,15 +1,28 @@
 import WeatherCard from "../WeatherCard/WeatherCard";
-import "./Main.css"
+import ItemCard from "../ItemCard/ItemCard";
+import { defaultClothingItems } from "../../utils/constants";
+import "./Main.css";
 
-function Main() {
-    return (<main>
-        <WeatherCard/>
-        <section className="cards">
-            <p className="cards__text">Today is 75&deg; F / You may want to wear:</p>
-            {/* Add Cards Here */}
-        </section>
-        </main>
-    )
+function Main({ weatherData }) {
+  return (
+    <main>
+      <WeatherCard />
+      <section className="cards">
+        <p className="cards__text">
+          Today is 75&deg; F / You may want to wear:
+        </p>
+        <ul className="cards__list">
+          {defaultClothingItems
+        //   .filter((item) => {
+        //     return item.weather === weatherData.type;
+        //   })
+          .map((item) => {
+            return <ItemCard key={item._id} item={item} />;
+          })}
+        </ul>
+      </section>
+    </main>
+  );
 }
 
 export default Main;
