bcfc56195994565c480980b3fce5e4252f4eba9e
BigGarrison <paxhuckstep@gmail.com>
2025-01-03 16:31:45 -0700

    14 minutes in part 14

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,13 +1,26 @@
-import "./WeatherCard.css"
-import sunny from "../../assets/sunny.png"
+import "./WeatherCard.css";
+import { weatherOptions } from "../../utils/constants";
+
+
 
 function WeatherCard({ weatherData }) {
+  
+  const filteredOptions = weatherOptions.filter((option) => {
     return (
-        <section className="weather-card">
-<p className="weather-card__temp">{weatherData.temp.F} &deg; F</p>
-<img src={sunny} alt="" className="weather-card__image"/>
-        </section>
-    )
+      weatherOptions.day === weatherData.isDay &&
+      option.condition === weatherData.conditon
+    );
+  });
+
+  console.log(weatherData, weatherOptions);
+  const weatherOptionUrl = filteredOptions[0]?.url;
+  
+  return (
+    <section className="weather-card">
+      <p className="weather-card__temp">{weatherData.temp.F} &deg; F</p>
+      <img src={weatherOptionUrl} alt="" className="weather-card__image" />
+    </section>
+  );
 }
 
 export default WeatherCard;
